0031fc235921bafb5132433b877931ccc4b74676
Juank628 <jsanchez_zunino@yahoo.com>
2022-12-08 15:44:35 -0500

    clear all completed tasks implemented

--- src/modules/Render.js
+++ src/modules/Render.js
@@ -9,13 +9,18 @@ export default class Render {
   show = (array) => {
     let index = 0;
     let checked = '';
+    let lineThrough = '';
     let listItemsHTML = '';
     array.forEach((task, i) => {
       index = i + 1;
       checked = '';
-      if (task.completed) checked = 'checked';
+      lineThrough = '';
+      if (task.completed) {
+        checked = 'checked';
+        lineThrough = 'style="text-decoration: line-through"';
+      }
       listItemsHTML += `
-          <li id=list-item-${index} key=${index}><input ${checked} data-index=${index} class="checkbox" type="checkbox"> <input id=description-${index} data-index=${index} class="task-description" value="${task.description}" disabled> <img id=more-${index} class="more-icon" data-index=${index} src=${moreIcon} alt=""><img id=delete-${index} class="delete-icon" data-index=${index} src=${deleteIcon} alt=""></li>
+          <li id=list-item-${index} key=${index}><input ${checked} data-index=${index} class="checkbox" type="checkbox"> <input id=description-${index} data-index=${index} ${lineThrough} class="task-description" value="${task.description}" disabled> <img id=more-${index} class="more-icon" data-index=${index} src=${moreIcon} alt=""><img id=delete-${index} class="delete-icon" data-index=${index} src=${deleteIcon} alt=""></li>
           `;
     });
     this.parentElement.innerHTML = listItemsHTML;
