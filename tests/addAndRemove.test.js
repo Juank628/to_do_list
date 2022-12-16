11b9c418e84c340a8934c02feee847bfd70cde9b
Paul Sáenz Sucre <paulsaenzsucre@outlook.com>
2022-12-16 10:21:22 -0500

    Add test remove item an render.

--- tests/addAndRemove.test.js
+++ tests/addAndRemove.test.js
@@ -60,4 +60,50 @@ describe('add and remove', () => {
     const listItems = document.querySelectorAll('.task-description');
     expect(listItems).toHaveLength(3);
   });
+
+  test('remove item', () => {
+    const collection = new Collection('tasks', [
+      {
+        description: 'first',
+        completed: false,
+        index: 1,
+      },
+      {
+        description: 'second',
+        completed: false,
+        index: 2,
+      },
+    ]);
+    
+    expect(collection.removeItem(1)).toStrictEqual([
+      {
+        description: 'second',
+        completed: false,
+        index: 1,
+      },
+    ]);
+  });
+
+  test('render after remove item', () => {
+    document.body.innerHTML = '<ul id="list"></ul>';
+    const list = document.getElementById('list');
+    const render = new Render(list);
+    const collection = new Collection('tasks', [
+      {
+        description: 'first',
+        completed: false,
+        index: 1,
+      },
+      {
+        description: 'second',
+        completed: false,
+        index: 2,
+      },
+    ]);
+
+    collection.removeItem(1);
+    render.show(collection.getItems());
+    const listItems = document.querySelectorAll('.task-description');
+    expect(listItems).toHaveLength(1);
+  });
 });
